2e5fb8c6757330fc318afa28dfc96fe3c7f47c68
Diego <diegoartiles13@gmail.com>
2019-12-22 11:22:24 -0300

    feat (services): add tags endpoints

--- src/app/services/general.service.ts
+++ src/app/services/general.service.ts
@@ -75,6 +75,12 @@ export class GeneralService {
   createTag(tagData: TagData): Promise<any> {
     return this.http.post(`${EnvironmentService.currentEnvironment.url}/services/entities/tags`, tagData).toPromise();
   }
+  editTag(tagData: TagData): Promise<any> {
+    return this.http.patch(`${EnvironmentService.currentEnvironment.url}/services/entities/tags`, tagData).toPromise();
+  }
+  deleteTag(id: number): Promise<any> {
+    return this.http.delete(`${EnvironmentService.currentEnvironment.url}/services/entities/tags/${id}`).toPromise();
+  }
   getTag(type: any): Promise<any> {
     return this.http.get(`${EnvironmentService.currentEnvironment.url}/services/entities/tags?q=tipoTagId=${type}` ).toPromise();
   }
